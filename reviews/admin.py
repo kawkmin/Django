a2fc2f13e177bfd6ae4deed4491d7dcb2d876bd0
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-04-02 14:28:49 +0900

    5.0

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from . import models
+
+
+@admin.register(models.Review)
+class ReviewAdmin(admin.ModelAdmin):
+
+    """Review"""
+
+    pass
+
 
 # Register your models here.
